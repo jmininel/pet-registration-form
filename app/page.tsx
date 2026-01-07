c35dc87255dcfe0b89bf7faa234ec6f3ff1b16b4
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2026-01-07 18:06:57 +0000

    feat(page): create main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,39 @@
 "use client"
 
-import { Header } from "./_components/header/header";
-//import { Button } from "./_components/ui/button";
-import { Input } from "./_components/ui/input";
+import { useForm } from "react-hook-form";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Header } from "./_components/header/Header";
+import { PetOwner } from "./_components/pet-owner-fields/PetOwner";
+import { PetField } from "./_components/pet-fields/PetField";
+import { PetScheduling } from "./_components/pet-scheduling/PetScheduling";
+import { petFormSchema, petFormType } from "./schema/pet-form/schema";
 
 export default function Home() {
+   const { register, handleSubmit } = useForm<petFormType>({
+    resolver: zodResolver(petFormSchema)
+  })
+
+  const handleSubmitPet = (data: petFormType) => {
+    return console.log(data)
+  }
+ 
   return (
-  <div className="bg-[#0b9280] h-screen flex justify-center" >
-   <div className="bg-[#F4F6F8]  rounded-2xl w-full max-w-6xl shadow-lg flex flex-col gap-9  mt-9">
-    <Header/>
-  
-    <div className="flex justify-between items-center  gap-4 px-10 ">
-      <label htmlFor="">nome
-       <Input className=" h-[50px] w-[500px]" type="text" placeholder="nome completo"/>
-       </label>
-      <Input className=" h-[50px] w-[500px]" type="text" placeholder="nome completo"/>
-     
-       
+    <div className="bg-[#0b9280] min-h-screen flex justify-center p-1" >
+      <div className="bg-[#F4F6F8] rounded-2xl w-full max-w-2xl md:max-w-6xl shadow-lg flex flex-col gap-9 mt-9">
+        <Header />
+        <form onSubmit={handleSubmit(handleSubmitPet)}>
+          <PetOwner register={register} />
+           <PetField register={register}/>
+           <PetScheduling />
+            <button
+            type="submit"
+            className=" bg-[#00C8B3] text-zinc-900 font-semibold px-6 py-3 rounded-xl ml-10 mt-6">
+            Enviar
+          </button>
+        </form>
+      </div>
     </div>
-  
-   </div>
-  </div>
   );
 }
